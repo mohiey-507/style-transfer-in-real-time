5aadb3f55788f6e48b60e1472bfc06a232d2e0be
mohiey <mohamedmohiey507@gmail.com>
2025-08-15 14:22:13 +0300

    Implemented set_seed

--- style_transfer/utils.py
+++ style_transfer/utils.py
@@ -1,4 +1,6 @@
 import torch
+import random
+import numpy as np
 
 def calc_mean_std(feat: torch.Tensor, eps: float = 1e-5):
     """Calculate channel-wise mean and standard deviation"""
@@ -7,3 +9,13 @@ def calc_mean_std(feat: torch.Tensor, eps: float = 1e-5):
     feat_std = feat_var.sqrt().view(N, C, 1, 1)
     feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
     return feat_mean, feat_std
+
+def set_seed(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
